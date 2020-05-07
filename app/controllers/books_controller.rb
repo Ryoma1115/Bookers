899cfb6b82045497a5cdf8556badcea650c309c9
高根陵平 <takaneryohei@takaneryoutairanoMacBook-Pro.local>
2020-05-07 21:01:40 +0900

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   
   def top
+
   end
 
   def index
@@ -33,7 +34,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
-       flash[:notice] = "Book was successfully updated."
+       flash[:notice] = "Book was successfully updated!!"
        redirect_to book_path(book.id)
     else
        @book = book
